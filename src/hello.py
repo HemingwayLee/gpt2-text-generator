1e44e1b281fe905eb9e0a2713fe3f3a2dd0e0dc9
HemingwayLee <coherence0815@gmail.com>
2023-11-15 17:58:34 +0900

    init project

---
+++ src/hello.py
@@ -0,0 +1,25 @@
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import keras_nlp
+import tensorflow as tf
+import keras_core as keras
+import time
+
+preprocessor = keras_nlp.models.GPT2CausalLMPreprocessor.from_preset(
+    "gpt2_base_en",
+    sequence_length=128,
+)
+gpt2_lm = keras_nlp.models.GPT2CausalLM.from_preset(
+    "gpt2_base_en", preprocessor=preprocessor
+)
+
+start = time.time()
+
+output = gpt2_lm.generate("My trip to Yosemite was", max_length=200)
+print("\nGPT-2 output:")
+print(output)
+
+end = time.time()
+print(f"TOTAL TIME ELAPSED: {end - start:.2f}s")
+
